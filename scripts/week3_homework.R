aaca8481ea178aafd8bbe244219175fcb81fb7de
Isaiah Thalmayer <ithalmayer@pointblue.org>
2021-10-07 23:23:05 -0700

    hw3 done

---
+++ scripts/week3_homework.R
@@ -0,0 +1,16 @@
+surveys <- read.csv("data/portal_data_joined.csv")
+surveys
+
+colnames(surveys)
+
+surveys_new <- surveys[1:60, c(6, 9, 13)]
+surveys_new
+
+surveys_new$species_id <- as.character(surveys_new$species_id)
+surveys_new$plot_type <- as.character(surveys_new$plot_type)
+
+surveys_new <- surveys_new[complete.cases(surveys_new), ]
+surveys_new
+
+challenge_base <- surveys_new[(surveys_new[, 2]>150),]
+challenge_base
